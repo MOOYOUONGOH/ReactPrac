2304f2c7cd8175716f7d43990f738dc71f7e6a88
unknown <moh@csumb.edu>
2019-05-30 18:40:49 -0700

    add delete

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
-
+import {HashRouter as Router, Route} from 'react-router-dom';
+import AppShell from './AppShell';
+import Home from './Home';
+import Text from './Text';
+import Words from './Words';
 class App extends React.Component{
     render() {
         return (
-            <div>
-                <h3> hello world</h3>
-            </div>
+        <Router>
+            <AppShell>
+                <div>
+                    <Route exact path="/" component={Home}/>
+                    <Route exact path="/texts" component={Text}/>
+                    <Route exact path="/words" component={Words}/>
+                </div>
+            </AppShell>
+            </Router>
         );
     }
 }
