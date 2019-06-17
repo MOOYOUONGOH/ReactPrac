2dcb2f197da9105fe254729ca7f980e0ed29971a
unknown <moh@csumb.edu>
2019-06-17 15:16:53 -0700

    restart Again!

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,17 @@
 import React from 'react';
-import {HashRouter as Router, Route} from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import AppShell from './AppShell';
-
+import Home from './Home';
 import Texts from './Texts';
 import Words from './Words';
-import Detail from './Detail';
 
-import Church from './Church';
-import Calendar from './Calendar';
-import Information from './Infomation';
-import Home from './GridView';
-import Gallary from './Gallary';
-
-
-class App extends React.Component{
+class App extends React.Component {
     render() {
         return (
-            <AppShell>
-                <div>
-                    <Route exact path="/" Component={Home}/>
-                    <Route exact path="/Church" Component={Church}/>
-                    <Route exact path="/Information" Component={Information}/>
-                    <Route exact path="/Calendar" Component={Calendar}/>
-                    <Route exact path="/Gallary" Component={Gallary}/>
-                </div>
-            </AppShell>
+            <p>123123</p>
         );
     }
 }
 
 export default App;
+
--- src/components/AppShell.js
+++ src/components/AppShell.js
@@ -1,253 +1,65 @@
 import React from 'react';
-import clsx from 'clsx';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
+import { Link as RouterLink } from 'react-router-dom';
+import Link from '@material-ui/core/Link';
+import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import MenuItem from '@material-ui/core/MenuItem';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import HomeIcon from '@material-ui/icons/Home';
-import DashboardIcon from '@material-ui/icons/Dashboard';
-import InfoIcon from '@material-ui/icons/Info';
-import PhotoIcon from '@material-ui/icons/Photo';
-import LocationIcon from '@material-ui/icons/Explore';
 
-import { Link as RouterLink } from 'react-router-dom';
-
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-
-const drawerWidth = 240;
-
-const useStyles = makeStyles(theme => ({
+const styles = {
   root: {
-    display: 'flex',
-  },
-  Pastor: {
-    width: '100%',
-    height: 'auto',
-  },  
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  appBarShift: {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
+    flexGrow: 1,
   },
   menuButton: {
-    marginRight: 36,
+    marginRight: 'auto'
   },
-  hide: {
-    display: 'none',
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: 'nowrap',
-  },
-  drawerOpen: {
-    width: drawerWidth,
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  drawerClose: {
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    overflowX: 'hidden',
-    width: theme.spacing(7) + 1,
-    [theme.breakpoints.up('sm')]: {
-      width: theme.spacing(9) + 1,
-    },
-  },
-  toolbar: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: '0 8px',
-    ...theme.mixins.toolbar,
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-  },
-}));
+};
 
-const menuIcons = [
-  {
-    index : '1',
-    text : 'Home'
-  },
-  {
-    index : '2',
-    text : 'About'
-  },
-  {
-    index : '3',
-    text : 'Photo'
-  },
-  {
-    index : '4',
-    text : 'Information'
-  },
-  {
-    index : '5',
-    text : 'Vision Trip'
-  }
-]
-
-
-class AppShell extends React.component {
+class AppShell extends React.Component {
   constructor(props) {
     super(props);
-  }    
-
-  render(){
-
-    const classes = useStyles();
-    const theme = useTheme();
-    const [open, setOpen] = React.useState(false);
-
-    function handleDrawerOpen() {
-      setOpen(true);
-    }
-
-    function handleDrawerClose() {
-      setOpen(false);
-    }
-
+    this.state = {
+      toggle: false
+    };
+  }
+  handleDrawerToggle = () => this.setState({ toggle: !this.state.toggle })
+  render() {
+    const { classes } = this.props;
     return (
-      <div className={classes.root}>
-        <CssBaseline />
-        <AppBar
-          position="fixed"
-          className={clsx(classes.appBar, {
-            [classes.appBarShift]: open,
-          })}
-        >
-          <Toolbar>
-            <IconButton
-              color="inherit"
-              aria-label="Open drawer"
-              onClick={handleDrawerOpen}
-              edge="start"
-              className={clsx(classes.menuButton, {
-                [classes.hide]: open,
-              })}
-            >
+      <div>
+        <div className={classes.root}>
+          <AppBar position="static">
+            <IconButton className={classes.menuButton} color="inherit" onClick={this.handleDrawerToggle}>
               <MenuIcon />
             </IconButton>
-            <Typography variant="h6" noWrap>
-              Monterey Hope Church 몬트레이 소망 교회
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <Drawer
-          variant="permanent"
-          className={clsx(classes.drawer, {
-            [classes.drawerOpen]: open,
-            [classes.drawerClose]: !open,
-          })}
-          classes={{
-            paper: clsx({
-              [classes.drawerOpen]: open,
-              [classes.drawerClose]: !open,
-            }),
-          }}
-          open={open}
-        >
-          <div className={classes.toolbar}>
-            <IconButton onClick={handleDrawerClose}>
-              {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-            </IconButton>
-          </div>
-          <Divider />
-          <List>
-          <Link component={RouterLink} to="/">
-            <ListItem button>
-              <ListItemIcon> 
-              <HomeIcon />             
-              </ListItemIcon>
-            <ListItemText primary="Home" />
-            </ListItem>
-          </Link>
-          <Link component={RouterLink} to="/Gallary">
-          <ListItem button>
-            <ListItemIcon>
-              <PhotoIcon />
-            </ListItemIcon>
-            <ListItemText primary="Photo" />
-          </ListItem>
-          </Link>
-          <Link component={RouterLink} to="/Calendar">
-          <ListItem button>
-            <ListItemIcon>
-              <DashboardIcon />
-            </ListItemIcon>
-            <ListItemText primary="Calender" />
-          </ListItem>
-          </Link>
-          <Link component={RouterLink} to="/Information">
-          <ListItem button>
-            <ListItemIcon>
-              <InfoIcon />
-            </ListItemIcon>
-            <ListItemText primary="Infomation" />
-          </ListItem>
-          </Link>
-          <Link component={RouterLink} to="/Church">
-          <ListItem button>
-            <ListItemIcon>
-              <LocationIcon />
-            </ListItemIcon>
-            <ListItemText primary="Location" />
-          </ListItem>
-          </Link>
-          </List>
-          <Divider />
-        </Drawer>
-        <main className={classes.content}>
-        <div className={classes.toolbar} />
-          <Grid container spacing={3}>
-            <Grid item xs={12}>
-              <Paper className={classes.paper}>
-                
-                <div id="content" style={{margin: 'auto', marginTop: '20px'}}>
-                  {React.cloneElement(this.props.children)}
-                </div>
-
-              </Paper>
-            </Grid>
-          </Grid>
-      </main>
+          </AppBar>
+          <Drawer open={this.state.toggle}>
+            <MenuItem onClick={this.handleDrawerToggle}>
+              <Link component={RouterLink} to="/">
+                Home
+</Link>
+            </MenuItem>
+            <MenuItem onClick={this.handleDrawerToggle}>
+              <Link component={RouterLink} to="/texts">
+                Texts
+</Link>
+            </MenuItem>
+            <MenuItem onClick={this.handleDrawerToggle}>
+              <Link component={RouterLink} to="/words">
+                Words
+</Link>
+            </MenuItem>
+          </Drawer>
+        </div>
+        <div id="content" style={{ margin: 'auto', marginTop: '20px' }}>
+          {React.cloneElement(this.props.children)}
+        </div>
       </div>
     );
-    }
+  }
 }
 
-export default AppShell;
+export default withStyles(styles)(AppShell);
+
